a88c27d3068c3e4ccae795d9b471b1fc86f337f6
Zafeer <contact@zafeer.pk>
2024-12-19 10:43:38 +0500

    chore: add more folders to look for tailwind classes to be made

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
+import "./styles/global.css";
+
 export default function App() {
   return (
     <View className="flex-1 justify-center items-center">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/*.{js,jsx,ts,tsx}", // Include files in the src folder
-    "./src/**/*.{js,jsx,ts,tsx}", // Include files in nested directories
-    "./src/**/**/*.{js,jsx,ts,tsx}", // Include files in deeply nested directories
+    "./src/*.{js,jsx,ts,tsx}",
+    "./src/screens/*.{js,jsx,ts,tsx}",
+    "./src/screens/**/*.{js,jsx,ts,tsx}",
+    "./src/components/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   presets: [require("nativewind/preset")],
   theme: {
